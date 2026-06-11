bee5cb7e657bb28910468dac19d32e142eb4f48d
Harrison O'Reibow <harrison.oreibow@gmail.com>
2026-06-11 18:02:49 +1200

    c: tidy, format

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -17,52 +18,53 @@ typedef struct {
     int param;
 } Command;
 
-
 TokenType makeToken(char c) {
     switch (c) {
-        case '>':
-            return DP_INC;
-            break;
-        case '<':
-            return DP_DEC;
-            break;
-        case '+':
-            return DATA_INC;
-            break;
-        case '-':
-            return DATA_DEC;
-            break;
-        case '[':
-            return JZ;
-            break;
-        case ']':
-            return JNZ;
-            break;
-        case '.':
-            return OUTPUT;
-            break;
-        case ',':
-            return INPUT;
-            break;
+    case '>':
+        return DP_INC;
+        break;
+    case '<':
+        return DP_DEC;
+        break;
+    case '+':
+        return DATA_INC;
+        break;
+    case '-':
+        return DATA_DEC;
+        break;
+    case '[':
+        return JZ;
+        break;
+    case ']':
+        return JNZ;
+        break;
+    case '.':
+        return OUTPUT;
+        break;
+    case ',':
+        return INPUT;
+        break;
+    default:
+        exit(EXIT_FAILURE);
     }
 }
 
-TokenType *tokenise(char* input, int inpLen) {
+TokenType *tokenise(char *input, size_t inpLen) {
     TokenType *tokBuf = malloc(inpLen * sizeof(TokenType));
-    for (int i = 0; i < inpLen; i++) {
+    for (size_t i = 0; i < inpLen; i++) {
         tokBuf[i] = makeToken(input[i]);
     }
     return tokBuf;
 }
 
 int main(int argc, char **argv) {
-    int inpLen = 8;
+    size_t inpLen = 8;
     char *inp = "><+-[],.";
     TokenType *toks = tokenise(inp, inpLen);
 
-    for (int i = 0; i < inpLen; i++) {
+    for (size_t i = 0; i < inpLen; i++) {
         int tok = toks[i];
         printf("%d\n", tok);
     }
-    return 0;
+    return EXIT_SUCCESS;
 }
